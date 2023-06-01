642ae9735cd3faf4d4804b7958ff4a072a9a422e
jvitorfarias <joaovitorfarias4@gmail.com>
2023-06-01 11:43:48 -0300

    commit final

---
+++ teste.js
@@ -0,0 +1,9 @@
+let primeiroNome = prompt("Insira seu primeiro nome");
+let segundoNome = prompt("Insira seu sobrenome");
+let nomeCompleto = primeiroNome + " " + segundoNome;
+
+let welcome = alert(`Seja bem vindo, ${nomeCompleto}!` );
+
+document.write(`Seja bem vindo, ${nomeCompleto}!`)
+
+console.log(nomeCompleto);
